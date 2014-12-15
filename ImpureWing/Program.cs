133d005cada186a2ccbddfe1c6c878f31a9d4077
Es_Program <radwimps.september@gmail.com>
2014-12-15 21:05:41 +0900

    first commit

---
+++ ImpureWing/Program.cs
@@ -0,0 +1,17 @@
+﻿using CoreTweet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImpureWing
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			var tokens = OAuth.Authorize("", "");
+		}
+	}
+}
